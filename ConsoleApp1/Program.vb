8055cbe9d5d97c429638227ac523c614a5243bbc
sairi muenala <ikaww@plaiaundi.net>
2020-03-31 12:55:22 +0200

    segundo commit

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -3,5 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+        Console.WriteLine("Desde el primer proyecto ")
     End Sub
 End Module
